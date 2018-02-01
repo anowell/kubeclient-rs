8b68c0e6d2aefdd9b5187470dd881f7db12ce87c
Dongsu Park <dongsu@kinvolk.io>
2018-02-01 11:37:48 +0100

    examples: update get-secret as well as list-nodes
    Make use of $KUBECONFIG env variable in example code like get-secret
    and list-nodes. If the variable is not set or is empty, we fall back to
    "admin.conf" like previously.

--- examples/list-nodes.rs
+++ examples/list-nodes.rs
@@ -1,9 +1,18 @@
 extern crate kubeclient;
 use kubeclient::prelude::*;
 use kubeclient::errors::*;
+use std::env;
 
 fn run_main() -> Result<i32> {
-    let kube = Kubernetes::load_conf("admin.conf")?;
+    // filename is set to $KUBECONFIG if the env var is available.
+    // Otherwise it falls back to "admin.conf".
+    let filename = env::var("KUBECONFIG").ok();
+    let filename = filename
+        .as_ref()
+        .map(String::as_str)
+        .and_then(|s| if s.is_empty() { None } else { Some(s) })
+        .unwrap_or("admin.conf");
+    let kube = Kubernetes::load_conf(filename)?;
 
     if kube.healthy()? {
         for node in kube.nodes().list(None)? {
@@ -16,8 +25,7 @@ fn run_main() -> Result<i32> {
 
 fn main() {
     match run_main() {
-        Ok(n)  => println!("Success error code is {}", n),
+        Ok(n) => println!("Success error code is {}", n),
         Err(e) => println!("Error: {}", e),
     }
 }
-
